1d59592d8a093d1043a6236a79df50aba85c57f4
Genesis Smothers <genesissmothers@Genesiss-MacBook-Pro.local>
2018-10-17 21:41:51 -0400

    I modified my file by adding more numbers to my ID. I believe that I might need to change this due to instruction 3 of the project.

--- MIPS1.asm
+++ MIPS1.asm
@@ -1,5 +1,7 @@
 .data
   id: .word 0
+  idn: .word 2
+  idnu: .word 8
 .text
   main: 
     li $v0, 11 #This tells the computer that I want to print a character 
@@ -8,4 +10,9 @@
     li $v0, 1 #This tells the computer that I want to print an integer 
     lw $a0, id
     syscall 
-    
+    li $v0, 1
+    lw $a0, idn
+    syscall 
+    li $v0, 1
+    lw $a0, idnu
+    syscall 
