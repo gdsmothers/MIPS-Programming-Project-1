1d5e0dd38ef4697229a4b49e3735f4287cd45ee1
Genesis Smothers <genesissmothers@Genesiss-MacBook-Pro.local>
2018-10-18 20:34:29 -0400

    I finished printing out my id number

--- MIPS1.asm
+++ MIPS1.asm
@@ -2,6 +2,11 @@
   id: .word 0
   idn: .word 2
   idnu: .word 8
+  idnum: .word 2
+  idnumb: .word 3
+  idnumbe: .word 8
+  idnumber: .word 3
+  idrebmun: .word 1
 .text
   main: 
     li $v0, 11 #This tells the computer that I want to print a character 
@@ -16,3 +21,26 @@
     li $v0, 1
     lw $a0, idnu
     syscall 
+    li $v0, 1
+    lw $a0,  idnum
+    syscall
+    li $v0, 1
+    lw $a0, idnumb
+    syscall 
+    li $v0, 1
+    lw $a0, idnumbe
+    syscall 
+    li $v0, 1
+    lw $a0, idnumber
+    syscall 
+    li $v0, 1
+    lw $a0, idrebmun
+    syscall 
+    
+
+
+
+
+
+
+
