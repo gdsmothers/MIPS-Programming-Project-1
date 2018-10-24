67ae111b5941af419305c74a711f9c13c2743686
Genesis <genesis.smothers@bison.howard.edu>
2018-10-23 22:06:59 -0400

    I am telling the computer that I want to print out integers so i have to subtract from the old $v0 to get 1 and then call my first two integers in my ID number
    I am telling the computer that I want to print out integers so i have to subtract from the old $v0 to get 1 and then call my first two integers in my ID number

--- MIPS1.asm
+++ MIPS1.asm
@@ -6,3 +6,8 @@ li $v0, 11 #This tells the computer that I want to print a character
 la $a0, 64 #64 @
 syscall 
 #ID Number
+sub $v0, $v0, 10 #This tells the computer that I want to print an integer by subtracting 10 from 11 to get 1 
+sub $a0, $a0, 64 #0 
+syscall 
+li $v0, 1
+addi $a0, $a0, 2 #2 
